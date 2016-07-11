e656001a2f9306be0b03486f81f97f1b0f3706da
Asaia Palacios <asaia.palacios@gmail.com>
2016-07-11 22:21:18 +0000

    config.py script to define variables

---
+++ api/config.py
@@ -0,0 +1,16 @@
+import os
+
+AIRBNB_ENV = os.environ.get('AIRBNB_ENV')
+
+if AIRBNB_ENV == "development": # add all variables we want to set
+    DEBUG = True
+    HOST = 'localhost'
+    PORT = 3333
+    DATABASE = {'host': '158.69.79.7', 'user': 'airbnb_user_dev', 'database': 'airbnb_dev', 'port': 3306, 'charset': 'utf8'}
+elif AIRBNB_ENV == "production":
+    DEBUG = False
+    HOST = '0.0.0.0'
+    PORT = 3000
+    DATABASE = {'host': '158.69.79.7', 'user': 'airbnb_user_prod', 'database': 'airbnb_prod', 'port': 3306, 'charset': 'utf8'}
+
+
