706c4ce701a3198fbc71c20ab3fc7145ad3045a3
Mason <mason.e.fish@gmail.com>
2016-07-23 12:58:03 -0700

    push sql script for database and user creation

--- sql/unit_test.sql
+++ sql/unit_test.sql
@@ -1,1 +1,11 @@
-CREATE USER Name
+CREATE USER 'airbnb_test_user'@'%' IDENTIFIED BY 'password';
+CREATE USER 'airbnb_test_user'@'127.0.0.1' IDENTIFIED BY 'password';
+
+CREATE DATABASE airbnb_test
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+GRANT ALL PRIVILEGES ON airbnb_test.*
+    TO 'airbnb_test_user'@'%' IDENTIFIED BY 'password';
+GRANT ALL PRIVILEGES ON airbnb_test.*
+    TO 'airbnb_test_user'@'127.0.0.1' IDENTIFIED BY 'password';
